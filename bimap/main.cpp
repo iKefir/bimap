326b6457e6574a73757762272f38b3d964e902ce
iKefir <danshk@mail.ru>
2016-09-18 14:57:47 +0300

    Initial Commit

---
+++ bimap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bimap
+//
+//  Created by Данил Шкарупин on 18.09.16.
+//  Copyright © 2016 Данил Шкарупин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
