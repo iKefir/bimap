b2e802709460fe0cff8808051e110248e6f1bbda
iKefir <danshk@mail.ru>
2016-09-21 22:12:57 +0300

    Done

---
+++ bimap/bimap.cpp
@@ -0,0 +1,402 @@
+//
+//  bimap.cpp
+//  bimap
+//
+//  Created by Данил Шкарупин on 18.09.16.
+//  Copyright © 2016 Данил Шкарупин. All rights reserved.
+//
+
+#include "bimap.hpp"
+#include <vector>
+using namespace std;
+
+bimap::left_iterator bimap::insert(bimap::left_t const& left, right_t const& right) {
+    
+    if (root -> left_left == nullptr) {
+        node* inserting = new node();
+        
+        inserting -> left_data = left;
+        inserting -> right_data = right;
+        inserting -> left_parent = root;
+        root -> left_left = inserting;
+        
+        inserting -> right_data = right;
+        inserting -> right_parent = root;
+        root -> right_left = inserting;
+        
+        left_iterator trying_it;
+        trying_it.current = inserting;
+        return trying_it;
+    }
+    
+    node *trying_left = root -> left_left, *trying_right = root -> right_left;
+    
+    int l_insert = 0, r_insert = 0;
+    
+    //find left
+    while (true) {
+        if (trying_left -> left_data == left) break;
+        if (trying_left -> left_data < left) {
+            if (trying_left -> left_right != nullptr) {
+                trying_left = trying_left -> left_right;
+            } else {
+                l_insert = 2;
+                break;
+            }
+        }
+        if (trying_left -> left_data > left) {
+            if (trying_left -> left_left != nullptr) {
+                trying_left = trying_left -> left_left;
+            } else {
+                l_insert = 1;
+                break;
+            }
+        }
+    }
+    
+    //find right
+    while (true) {
+        if (trying_right -> right_data == right) break;
+        if (trying_right -> right_data < right) {
+            if (trying_right -> right_right != nullptr) {
+                trying_right = trying_right -> right_right;
+            } else {
+                r_insert = 2;
+                break;
+            }
+        }
+        if (trying_right -> right_data > right) {
+            if (trying_right -> right_left != nullptr) {
+                trying_right = trying_right -> right_left;
+            } else {
+                r_insert = 1;
+                break;
+            }
+        }
+    }
+    
+    //if good - insert
+    if (l_insert != 0 && r_insert != 0) {
+        node* inserting = new node();
+        
+        inserting -> left_data = left;
+        inserting -> right_data = right;
+        inserting -> left_parent = trying_left;
+        (l_insert == 1 ? trying_left -> left_left : trying_left -> left_right) = inserting;
+        
+        inserting -> right_data = right;
+        inserting -> right_parent = trying_right;
+        (r_insert == 1 ? trying_right -> right_left : trying_right -> right_right) = inserting;
+        
+        left_iterator trying_it;
+        trying_it.current = inserting;
+        return trying_it;
+    } else {
+        return end_left();
+    }
+}
+
+void bimap::erase(bimap::left_iterator it) {
+    if (it.current -> left_parent != nullptr) {
+        bool l_parent, r_parent;
+        if (it.current -> left_parent -> left_left == it.current) l_parent = 0;
+        else l_parent = 1;
+        if (it.current -> right_parent -> right_left == it.current) r_parent = 0;
+        else r_parent = 1;
+        //Если нет детей -- всё хорошо
+        if (it.current -> left_left == nullptr && it.current -> left_right == nullptr) {
+            (l_parent == false ? it.current -> left_parent -> left_left: it.current -> left_parent -> left_right) = nullptr;
+        }
+        //Если два ребенка -- подвесить левого вместо current правого к самому большому ребенку левого
+        else if (it.current -> left_left != nullptr && it.current -> left_right != nullptr) {
+            (l_parent == false ? it.current -> left_parent -> left_left : it.current -> left_parent -> left_right) = it.current -> left_left;
+            it.current -> left_left -> left_parent = it.current -> left_parent;
+            
+            node* trying = it.current -> left_left;
+            while (trying -> left_right != nullptr) trying = trying -> left_right;
+            
+            trying -> left_right = it.current -> left_right;
+            it.current -> left_right -> left_parent = trying;
+        }
+        //Если один ребенок -- подвесить вместо current
+        else {
+            (l_parent == false ? it.current -> left_parent -> left_left : it.current -> left_parent -> left_right) = (it.current -> left_left != nullptr ? it.current -> left_left : it.current -> left_right);
+            (it.current -> left_left != nullptr ? it.current -> left_left : it.current -> left_right) -> left_parent = it.current -> left_parent;
+        }
+        
+        
+        if (it.current -> right_left == nullptr && it.current -> right_right == nullptr) {
+            (r_parent == false ? it.current -> right_parent -> right_left: it.current -> right_parent -> right_right) = nullptr;
+        }
+        //Если два ребенка -- подвесить левого вместо current правого к самому большому ребенку левого
+        else if (it.current -> right_left != nullptr && it.current -> right_right != nullptr) {
+            (r_parent == false ? it.current -> right_parent -> right_left : it.current -> right_parent -> right_right) = it.current -> right_left;
+            it.current -> right_left -> right_parent = it.current -> right_parent;
+            
+            node* trying = it.current -> right_left;
+            while (trying -> right_right != nullptr) trying = trying -> right_right;
+            
+            trying -> right_right = it.current -> right_right;
+            it.current -> right_right -> right_parent = trying;
+        }
+        //Если один ребенок -- подвесить вместо current
+        else {
+            (r_parent == false ? it.current -> right_parent -> right_left : it.current -> right_parent -> right_right) = (it.current -> right_left != nullptr ? it.current -> right_left : it.current -> right_right);
+            (it.current -> right_left != nullptr ? it.current -> right_left : it.current -> right_right) -> right_parent = it.current -> right_parent;
+        }
+        
+        delete it.current;
+    }
+}
+void bimap::erase(bimap::right_iterator it) {
+    erase(it.flip());
+}
+
+bimap::left_iterator  bimap::find_left (bimap::left_t  const& left)  const {
+    
+    if (root -> left_left == nullptr) return end_left();
+    
+    node *trying = root -> left_left;
+    
+    while (true) {
+        if (trying -> left_data == left) break;
+        if (trying -> left_data < left) {
+            if (trying -> left_right != nullptr) {
+                trying = trying -> left_right;
+            } else {
+                return end_left();
+            }
+        }
+        if (trying -> left_data > left) {
+            if (trying -> left_left != nullptr) {
+                trying = trying -> left_left;
+            } else {
+                return end_left();
+            }
+        }
+    }
+    
+    left_iterator trying_it;
+    trying_it.current = trying;
+    return trying_it;
+}
+bimap::right_iterator bimap::find_right(bimap::right_t const& right) const {
+    
+    if (root -> right_left == nullptr) return end_right();
+    
+    node *trying = root -> right_left;
+    
+    while (true) {
+        if (trying -> right_data == right) break;
+        if (trying -> right_data < right) {
+            if (trying -> right_right != nullptr) {
+                trying = trying -> right_right;
+            } else {
+                return end_right();
+            }
+        }
+        if (trying -> right_data > right) {
+            if (trying -> right_left != nullptr) {
+                trying = trying -> right_left;
+            } else {
+                return end_right();
+            }
+        }
+    }
+    
+    right_iterator trying_it;
+    trying_it.current = trying;
+    return trying_it;
+}
+
+bimap::left_iterator bimap::begin_left() const {
+    if (root -> left_left == nullptr) return end_left();
+    node* trying = root -> left_left;
+    while (trying -> left_left != nullptr) trying = trying -> left_left;
+    
+    left_iterator trying_it;
+    trying_it.current = trying;
+    return trying_it;
+}
+bimap::left_iterator bimap::end_left() const {
+    left_iterator left;
+    left.current = root;
+    return left;
+}
+
+bimap::right_iterator bimap::begin_right() const {
+    if (root -> right_left == nullptr) return end_right();
+    node* trying = root -> right_left;
+    while (trying -> right_left != nullptr) trying = trying -> right_left;
+    
+    right_iterator trying_it;
+    trying_it.current = trying;
+    return trying_it;
+}
+bimap::right_iterator bimap::end_right() const {
+    right_iterator right;
+    right.current = root;
+    return right;
+}
+
+bimap::bimap() {
+    root = new node();
+}
+
+void bimap::clear_all(bimap::node* current) {
+    if (current -> left_left != nullptr) {
+        clear_all(current -> left_left);
+    }
+    if (current -> left_right != nullptr) {
+        clear_all(current -> left_right);
+    }
+    delete(current);
+}
+
+bimap::~bimap() {
+    if (root -> left_left != nullptr) clear_all(root -> left_left);
+    delete(root);
+}
+
+bimap::left_t const& bimap::left_iterator::operator*() const {
+    return current -> left_data;
+}
+
+bimap::left_iterator& bimap::left_iterator::operator++() {
+    if (current -> left_right != nullptr) {
+        current = current -> left_right;
+        while (current -> left_left != nullptr) current = current -> left_left;
+    } else {
+        while ((current -> left_parent != nullptr) && (current -> left_parent -> left_right == current)) current = current -> left_parent;
+        if (current -> left_parent != nullptr) current = current -> left_parent;
+    }
+    return *this;
+}
+bimap::left_iterator bimap::left_iterator::operator++(int) {
+    left_iterator res(*this);
+    if (current -> left_right != nullptr) {
+        current = current -> left_right;
+        while (current -> left_left != nullptr) current = current -> left_left;
+    } else {
+        while ((current -> left_parent != nullptr) && (current -> left_parent -> left_right == current)) current = current -> left_parent;
+        if (current -> left_parent != nullptr) current = current -> left_parent;
+    }
+    return res;
+}
+
+bimap::left_iterator& bimap::left_iterator::operator--() {
+    if (current -> left_left != nullptr) {
+        current = current -> left_left;
+        while (current -> left_right != nullptr) current = current -> left_right;
+    } else {
+        while ((current -> left_parent != nullptr) && (current -> left_parent -> left_left == current)) current = current -> left_parent;
+        if (current -> left_parent != nullptr) current = current -> left_parent;
+    }
+    return *this;
+}
+bimap::left_iterator bimap::left_iterator::operator--(int) {
+    left_iterator res(*this);
+    if (current -> left_left != nullptr) {
+        current = current -> left_left;
+        while (current -> left_right != nullptr) current = current -> left_right;
+    } else {
+        while ((current -> left_parent != nullptr) && (current -> left_parent -> left_left == current)) current = current -> left_parent;
+        if (current -> left_parent != nullptr) current = current -> left_parent;
+    }
+    return res;
+}
+
+bimap::right_iterator bimap::left_iterator::flip() const {
+    right_iterator right;
+    right.current = current;
+    return right;
+}
+
+bool bimap::left_iterator::operator==(const left_iterator right) const {
+    return current == right.current;
+}
+bool bimap::left_iterator::operator!=(const left_iterator right) const {
+    return current != right.current;
+}
+
+bimap::right_t const& bimap::right_iterator::operator*() const {
+    return current -> right_data;
+}
+
+bimap::right_iterator& bimap::right_iterator::operator++() {
+    if (current -> right_right != nullptr) {
+        current = current -> right_right;
+        while (current -> right_left != nullptr) current = current -> right_left;
+    } else {
+        while ((current -> right_parent != nullptr) && (current -> right_parent -> right_right == current)) current = current -> right_parent;
+        if (current -> right_parent != nullptr) current = current -> right_parent;
+    }
+    return *this;
+}
+bimap::right_iterator bimap::right_iterator::operator++(int) {
+    right_iterator res(*this);
+    if (current -> right_right != nullptr) {
+        current = current -> right_right;
+        while (current -> right_left != nullptr) current = current -> right_left;
+    } else {
+        while ((current -> right_parent != nullptr) && (current -> right_parent -> right_right == current)) current = current -> right_parent;
+        if (current -> right_parent != nullptr) current = current -> right_parent;
+    }
+    return res;
+}
+
+bimap::right_iterator& bimap::right_iterator::operator--() {
+    if (current -> right_left != nullptr) {
+        current = current -> right_left;
+        while (current -> right_right != nullptr) current = current -> right_right;
+    } else {
+        while ((current -> right_parent != nullptr) && (current -> right_parent -> right_left == current)) current = current -> right_parent;
+        if (current -> right_parent != nullptr) current = current -> right_parent;
+    }
+    return *this;
+}
+bimap::right_iterator bimap::right_iterator::operator--(int) {
+    right_iterator res(*this);
+    if (current -> right_left != nullptr) {
+        current = current -> right_left;
+        while (current -> right_right != nullptr) current = current -> right_right;
+    } else {
+        while ((current -> right_parent != nullptr) && (current -> right_parent -> right_left == current)) current = current -> right_parent;
+        if (current -> right_parent != nullptr) current = current -> right_parent;
+    }
+    return res;
+}
+
+bimap::left_iterator bimap::right_iterator::flip() const {
+    left_iterator left;
+    left.current = current;
+    return left;
+}
+
+bool bimap::right_iterator::operator==(const right_iterator right) const {
+    return current == right.current;
+}
+bool bimap::right_iterator::operator!=(const right_iterator right) const {
+    return current != right.current;
+}
+
+int main() {
+    bimap a;
+    int sch = 0, genered;
+    vector < int > data;
+    for (int i = 0; i < 50000; i++) {
+        genered = rand();
+        data.push_back(genered);
+        a.insert(genered, rand());
+    }
+    int ind = 0;
+    bimap::left_iterator itt;
+    while (a.begin_left() != a.end_left()) {
+        itt = a.find_left(data[ind]);
+        a.erase(itt);
+        ++ind;
+        ++sch;
+    }
+    
+    cout << sch << endl;
+}
---
+++ bimap/bimap.hpp
@@ -0,0 +1,142 @@
+//
+//  bimap.hpp
+//  bimap
+//
+//  Created by Данил Шкарупин on 18.09.16.
+//  Copyright © 2016 Данил Шкарупин. All rights reserved.
+//
+
+#ifndef bimap_hpp
+#define bimap_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+
+struct bimap
+{
+    
+    // Вы можете определить эти тайпдефы по вашему усмотрению.
+    typedef int left_t;
+    typedef int right_t;
+    
+    struct left_iterator;
+    struct right_iterator;
+    struct node;
+    
+//    friend left_iterator;
+//    friend right_iterator;
+    
+    // Создает bimap не содержащий ни одной пары.
+    bimap();
+    
+    // Деструктор. Вызывается при удалении объектов bimap.
+    // Инвалидирует все итераторы ссылающиеся на элементы этого bimap
+    // (включая итераторы ссылающиеся на элементы следующие за последними).
+    void clear_all(node*);
+    ~bimap();
+    
+    // Вставка пары (left, right), возвращает итератор на left.
+    // Если такой left или такой right уже присутствуют в bimap, вставка не
+    // производится и возвращается end_left().
+    left_iterator insert(left_t const& left, right_t const& right);
+    
+    // Удаляет элемент и соответствующий ему парный.
+    // erase невалидного итератора неопределен.
+    // erase(end_left()) и erase(end_right()) неопределены.
+    // Пусть it ссылается на некоторый элемент e.
+    // erase инвалидирует все итераторы ссылающиеся на e и на элемент парный к e.
+    void erase(left_iterator it);
+    void erase(right_iterator it);
+    
+    // Возвращает итератор по элементу. В случае если элемент не найден, возвращает
+    // end_left()/end_right() соответственно.
+    left_iterator  find_left (left_t  const& left)  const;
+    right_iterator find_right(right_t const& right) const;
+    
+    // Возващает итератор на минимальный по величине left.
+    left_iterator begin_left() const;
+    // Возващает итератор на следующий за последним по величине left.
+    left_iterator end_left() const;
+    
+    // Возващает итератор на минимальный по величине right.
+    right_iterator begin_right() const;
+    // Возващает итератор на следующий за последним по величине right.
+    right_iterator end_right() const;
+    
+private:
+    node* root;
+};
+
+struct bimap::left_iterator
+{
+    
+    friend struct bimap;
+    // Элемент на который сейчас ссылается итератор.
+    // Разыменование итератора end_left() неопределено.
+    // Разыменование невалидного итератора неопределено.
+    left_t const& operator*() const;
+    
+    // Переход к следующему по величине left'у.
+    // Инкремент итератора end_left() неопределен.
+    // Инкремент невалидного итератора неопределен.
+    left_iterator& operator++();
+    left_iterator operator++(int);
+    
+    // Переход к предыдущему по величине left'у.
+    // Декремент итератора begin_left() неопределен.
+    // Декремент невалидного итератора неопределен.
+    left_iterator& operator--();
+    left_iterator operator--(int);
+    
+    // left_iterator ссылается на левый элемент некоторой пары.
+    // Эта функция возвращает итератор на правый элемент той же пары.
+    // end_left().flip() возращает end_right().
+    // end_right().flip() возвращает end_left().
+    // flip() невалидного итератора неопределен.
+    right_iterator flip() const;
+    
+    bool operator==(const left_iterator) const;
+    bool operator!=(const left_iterator) const;
+    
+private:
+    node* current;
+};
+
+struct bimap::right_iterator
+{
+    
+    friend struct bimap;
+    // Здесь всё аналогично left_iterator.
+    right_t const& operator*() const;
+    
+    right_iterator& operator++();
+    right_iterator operator++(int);
+    
+    right_iterator& operator--();
+    right_iterator operator--(int);
+    
+    left_iterator flip() const;
+    
+    bool operator==(const right_iterator) const;
+    bool operator!=(const right_iterator) const;
+    
+private:
+    node* current;
+};
+
+struct bimap::node
+{
+    bimap::left_t  left_data;
+    node*   left_left;
+    node*   left_right;
+    node*   left_parent;
+    
+    bimap::right_t right_data;
+    node*   right_left;
+    node*   right_right;
+    node*   right_parent;
+};
+
+
+#endif /* bimap_hpp */
